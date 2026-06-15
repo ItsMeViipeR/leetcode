edd11e848fe4d5f228bb1456daae141416c71fe7
ItsMeViipeR <itsmeviiper@gmail.com>
2026-06-15 11:48:30 +0200

    fix: optimized version of "Two Sum"

--- two_sum.go
+++ two_sum.go
@@ -1,12 +1,14 @@
 package main
 
 func twoSum(nums []int, target int) []int {
+	seen := make(map[int]int)
+
 	for i, num := range nums {
-		for j := i + 1; j < len(nums); j++ {
-			if num+nums[j] == target {
-				return []int{i, j}
-			}
+		complement := target - num
+		if idx, found := seen[complement]; found {
+			return []int{idx, i}
 		}
+		seen[num] = i
 	}
 	return nil
 }
