6676c4e39cfae9fd3e9133eb4ade1423a4f87776
ItsMeViipeR <itsmeviiper@gmail.com>
2026-06-15 11:54:57 +0200

    feat: first version of "Palindrome Number"

---
+++ palindrome_numer.go
@@ -0,0 +1,38 @@
+package main
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+
+	original := x
+	reversed := 0
+	for x != 0 {
+		reversed = reversed*10 + x%10
+		x /= 10
+	}
+
+	if original != reversed {
+		return false
+	}
+
+	return true
+}
+
+func main() {
+	x := 121
+	result := isPalindrome(x)
+	println(result)
+
+	x = -121
+	result = isPalindrome(x)
+	println(result)
+
+	x = 123
+	result = isPalindrome(x)
+	println(result)
+
+	x = 32123
+	result = isPalindrome(x)
+	println(result)
+}
